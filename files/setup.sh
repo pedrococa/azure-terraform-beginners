95ae2189996c8dd51ec6ba2fb6c62632a33e7c21
Pedro <pedro.coca@gmail.com>
2018-11-12 21:28:10 +0000

    Changed pic to trigger a deployment automatically

--- files/setup.sh
+++ files/setup.sh
@@ -7,7 +7,7 @@ apt install -y apache2 > /dev/null 2>&1
 cat << EOM > /var/www/html/index.html
 <html>
   <head><title>RRAAS</title></head>
-  <center><img src="https://configure-12.rolls-roycemotorcars.com/html5/assets/images/model-selection/RR_Phantom_Model_Selector_Belladonna_3.jpg"></img></center>
+  <center><img src="https://configure-12.rolls-roycemotorcars.com/html5/assets/images/model-selection/RR_Model_Selector_Ghost_SWB_Light.jpg"></img></center>
   <marquee><h1>RRAAS!</h1></marquee>
   </body>
 </html>
