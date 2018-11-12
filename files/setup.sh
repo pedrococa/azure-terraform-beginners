5cfb9527159d29ee4c2a13210fe4b1f6d9427804
Pedro <pedro.coca@gmail.com>
2018-11-12 20:20:32 +0000

    Modify web image, background, title, etc

--- files/setup.sh
+++ files/setup.sh
@@ -7,10 +7,9 @@ apt install -y apache2 > /dev/null 2>&1
 
 cat << EOM > /var/www/html/index.html
 <html>
-  <head><title>Meow!</title></head>
-  <body style="background-image: linear-gradient(red,orange,yellow,green,blue,indigo,violet);">
-  <center><img src="http://placekitten.com/800/600"></img></center>
-  <marquee><h1>Meow World</h1></marquee>
+  <head><title>RRAAS</title></head>
+  <center><img src="https://configure-12.rolls-roycemotorcars.com/html5/assets/images/model-selection/RR_Phantom_Model_Selector_Belladonna_3.jpg"></img></center>
+  <marquee><h1>RRAAS!</h1></marquee>
   </body>
 </html>
 EOM
