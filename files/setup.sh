d26e19bbc1b6e4921ecc59cc6d113438cf56d745
Pedro <1758302+pedrococa@users.noreply.github.com>
2019-01-09 14:40:31 +0000

    Update demo image
    Default demo image

--- files/setup.sh
+++ files/setup.sh
@@ -7,7 +7,7 @@ apt install -y apache2 > /dev/null 2>&1
 cat << EOM > /var/www/html/index.html
 <html>
   <head><title>World Map... as a service</title></head>
-  <center><img src="https://geospatialmedia.s3.amazonaws.com/wp-content/uploads/2018/04/GeoSpock_Logo-Registered-Black.jpg"></img></center>
+  <center><img src="http://econscribi.com/wp-content/uploads/2014/11/shutterstock_129655604-Converted-copy-text.png"></img></center>
   </body>
 </html>
 EOM
