2454587ee2dad51a637eb5cc3052f95ca8279bb4
Pedro <1758302+pedrococa@users.noreply.github.com>
2019-07-02 09:44:33 +0100

    Update setup.sh

--- files/setup.sh
+++ files/setup.sh
@@ -7,7 +7,7 @@ apt install -y apache2 > /dev/null 2>&1
 cat << EOM > /var/www/html/index.html
 <html>
   <head><title>World Map... as a service</title></head>
-  <center><img src="http://econscribi.com/wp-content/uploads/2014/11/shutterstock_129655604-Converted-copy-text.png"></img></center>
+  <center><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Floppy_disk_300_dpi.jpg/571px-Floppy_disk_300_dpi.jpg"></img></center>
   </body>
 </html>
 EOM
