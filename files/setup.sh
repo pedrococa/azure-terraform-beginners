4e3c8e3eaeb878fe295e0d8c344783002bc73ffb
Pedro <1758302+pedrococa@users.noreply.github.com>
2019-08-13 11:20:26 +0100

    Updated image
    Live demo image

--- files/setup.sh
+++ files/setup.sh
@@ -7,7 +7,7 @@ apt install -y apache2 > /dev/null 2>&1
 cat << EOM > /var/www/html/index.html
 <html>
   <head><title>World Map... as a service</title></head>
-  <center><img src="https://images-na.ssl-images-amazon.com/images/I/91%2Bl%2B4-P7jL._SX355_.jpg"></img></center>
+  <center><img src="https://linpack-for-tableau.com/uploads/actualites/livedemo-1.png"></img></center>
   </body>
 </html>
 EOM
